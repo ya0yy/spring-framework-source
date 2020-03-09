d08ed88b5fdae7a36e56a8cc14c978ba45ab6bc1
yaoyy <tuzkiyao@gmail.com>
2020-03-10 00:50:08 +0800

    @Bean初始化bean补充

---
+++ main1/src/main/java/com/yao/bean的初始化方法/TestFactoryBean.java
@@ -0,0 +1,48 @@
+package com.yao.bean的初始化方法;
+
+import org.springframework.beans.factory.FactoryBean;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+
+/**
+ * 　　　　　　　 ┏┓　 ┏┓+ +
+ * 　　　　　　　┏┛┻━━━┛┻┓ + +
+ * 　　　　　　　┃　　　　　　┃
+ * 　　　　　　　┃　　　━　　 ┃ ++ + + +
+ * 　　　　　　 ████━████  ┃+
+ * 　　　　　　　┃　　　　　　　┃ +
+ * 　　　　　　　┃　　　┻　　　┃
+ * 　　　　　　　┃　　　　　　┃ + +
+ * 　　　　　　　┗━┓　　　┏━┛
+ * 　　　　　　　　 ┃　　　┃
+ * 　　　　　　　　 ┃　　　┃ + + + +
+ * 　　　　　　　　 ┃　　　┃　　　　Code is far away from bug with the animal protecting
+ * 　　　　　　　　 ┃　　　┃ + 　　　　神兽保佑,代码无bug
+ * 　　　　　　　　 ┃　　　┃
+ * 　　　　　　　　 ┃　　　┃　　+
+ * 　　　　　　　　 ┃　 　 ┗━━━┓ + +
+ * 　　　　　　　　 ┃ 　　　　   ┣┓
+ * 　　　　　　　　 ┃ 　　　　　 ┏┛
+ * 　　　　　　　　 ┗┓┓┏━┳┓┏┛ + + + +
+ * 　　　　　　　　  ┃┫┫ ┃┫┫
+ * 　　　　　　　　  ┗┻┛ ┗┻┛+ + + +
+ * <p>
+ * spring
+ * 2020-03-09 19:12
+ *
+ * @author yaoyy
+ */
+@Component
+public class TestFactoryBean implements FactoryBean {
+
+	@Override
+	public Object getObject() throws Exception {
+		return new ArrayList(8);
+	}
+
+	@Override
+	public Class<?> getObjectType() {
+		return ArrayList.class;
+	}
+}
---
+++ main1/src/main/java/com/yao/bean的初始化方法/TestMethodBean.java
@@ -0,0 +1,35 @@
+package com.yao.bean的初始化方法;
+
+/**
+ * 　　　　　　　 ┏┓　 ┏┓+ +
+ * 　　　　　　　┏┛┻━━━┛┻┓ + +
+ * 　　　　　　　┃　　　　　　┃
+ * 　　　　　　　┃　　　━　　 ┃ ++ + + +
+ * 　　　　　　 ████━████  ┃+
+ * 　　　　　　　┃　　　　　　　┃ +
+ * 　　　　　　　┃　　　┻　　　┃
+ * 　　　　　　　┃　　　　　　┃ + +
+ * 　　　　　　　┗━┓　　　┏━┛
+ * 　　　　　　　　 ┃　　　┃
+ * 　　　　　　　　 ┃　　　┃ + + + +
+ * 　　　　　　　　 ┃　　　┃　　　　Code is far away from bug with the animal protecting
+ * 　　　　　　　　 ┃　　　┃ + 　　　　神兽保佑,代码无bug
+ * 　　　　　　　　 ┃　　　┃
+ * 　　　　　　　　 ┃　　　┃　　+
+ * 　　　　　　　　 ┃　 　 ┗━━━┓ + +
+ * 　　　　　　　　 ┃ 　　　　   ┣┓
+ * 　　　　　　　　 ┃ 　　　　　 ┏┛
+ * 　　　　　　　　 ┗┓┓┏━┳┓┏┛ + + + +
+ * 　　　　　　　　  ┃┫┫ ┃┫┫
+ * 　　　　　　　　  ┗┻┛ ┗┻┛+ + + +
+ * <p>
+ * spring
+ * 2020-03-09 23:54
+ *
+ * @author yaoyy
+ */
+public class TestMethodBean {
+	public TestMethodBean() {
+		System.out.println("TestMethodBean 构造器");
+	}
+}
